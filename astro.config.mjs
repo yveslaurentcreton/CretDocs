e43e6b4fdc83ff654bad89a38cf6d45dd726a6ef
Yves-Laurent Creton <52154171+yveslaurentcreton@users.noreply.github.com>
2024-09-04 13:31:57 +0200

    Change site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,9 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'DocsJet',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: 'https://github.com/yveslaurentcreton/docsjet',
 			},
 			sidebar: [
 				{
