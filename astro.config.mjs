bac1e844339e646057f4016578596f212339bcf7
Yves-Laurent Creton <52154171+yveslaurentcreton@users.noreply.github.com>
2024-09-05 13:40:48 +0200

    Fix css

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://yveslaurentcreton.github.io',
-	base: '/docsjet',
+	base: '/DocsJet',
 	integrations: [
 		starlight({
 			title: 'DocsJet',
