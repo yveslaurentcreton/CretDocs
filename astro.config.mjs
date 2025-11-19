b012d220070fe5c7a53d13f3b416d518b8ad1be1
Yves-Laurent Creton <52154171+yveslaurentcreton@users.noreply.github.com>
2025-11-19 09:24:34 +0100

    feat: modify k3s cluster installation guide and integrate Mermaid diagram support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import mermaid from 'astro-mermaid';
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,6 +10,7 @@ export default defineConfig({
 		host: true
 	},
 	integrations: [
+		mermaid(),
 		starlight({
 			title: 'CretDocs',
 			social: [
@@ -53,7 +55,7 @@ export default defineConfig({
 						{
 							label: 'Powershell Modules',
 							link: 'toolbox/powershell-modules',
-						},						{
+						}, {
 							label: 'Resources',
 							link: 'toolbox/resources',
 						},
